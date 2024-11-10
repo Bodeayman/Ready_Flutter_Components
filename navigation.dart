99925d355d8411c7ab23ffc759df434176ce0389
Abdullah Ayman <104903899+Bodeayman@users.noreply.github.com>
2024-11-10 21:53:40 +0200

    Update navigation.dart

--- navigation.dart
+++ navigation.dart
@@ -1,16 +1,16 @@
-   return MaterialApp(
-      home: Register(),
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/login.dart': (context) => Login(),
-        '/register.dart': (context) => Register(),
-        //These are the routes of teh prograph
-      },
-    );
+   // return MaterialApp(
+   //    home: Register(),
+   //    debugShowCheckedModeBanner: false,
+   //    routes: {
+   //      '/login.dart': (context) => Login(),
+   //      '/register.dart': (context) => Register(),
+   //      //These are the routes of teh prograph
+   //    },
+   //  );
 // Main file 
 
 onPressed: () {
-    Navigator.push(
+    Navigator.pushReplacement(
       context,
       MaterialPageRoute(
         builder: (context) => const Login(),
