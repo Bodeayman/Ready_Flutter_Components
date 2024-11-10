01e1dc38bb955580352643dc5c77fd7f5c2a4a8f
Abdullah Ayman <104903899+Bodeayman@users.noreply.github.com>
2024-11-10 21:51:07 +0200

    Create navigation.dart

---
+++ navigation.dart
@@ -0,0 +1,21 @@
+   return MaterialApp(
+      home: Register(),
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/login.dart': (context) => Login(),
+        '/register.dart': (context) => Register(),
+        //These are the routes of teh prograph
+      },
+    );
+// Main file 
+
+onPressed: () {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const Login(),
+      ),
+    );
+  },
+// This is for each button 
+// The names is useless for reference
