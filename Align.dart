e7a9dfd2b8b0191a8fa7b4fa27084913e0b83ffa
Abdullah Ayman <104903899+Bodeayman@users.noreply.github.com>
2024-11-11 20:30:50 +0200

    Create Align.dart

---
+++ Align.dart
@@ -0,0 +1,10 @@
+    SizedBox(
+              width: double.infinity,
+              child: Text(
+                "Details",
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.start,
+                //Doesn't work alone in the star
+              ),
+            )
+      //You should know to make the textAlign work in the start, make the width like this , and put it inside the SizedBox
